fd39463a9fa964281285b072d14ec3c27cafd046
CptCaveira <janos.rocha@gmail.com>
2021-03-03 17:41:22 +0100

    added articles - url or index

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
 
@@ -7,4 +8,16 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def wiki(request, article):
+    
+    md = markdown.Markdown()
+    text = util.get_entry(article)
+    split = text.split()
+    title = split[1]  
+    return render(request, "encyclopedia/wiki.html", {
+        "text": md.convert(text),
+        "title" : title
+    }) 
+    
 
